140e41888948c83a83b9ea96f50d7856facd9267
sitizahrafazria <stzhrafzria@gmail.com>
2025-09-26 10:53:12 +0700

    proses login

--- login.php
+++ login.php
@@ -1,3 +1,24 @@
+<?php
+    session_start();
+
+    // Proses login saat form dikirim
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $username = $_POST['username'] ?? '';
+        $password = $_POST['password'] ?? '';
+
+        // Login sederhana (username: admin, password: 123)
+        if ($username === 'admin' && $password === '123') {
+        $_SESSION['username'] = $username;
+        $_SESSION['role'] = 'Dosen';
+        header("Location: dashboard.php");
+        exit;
+        } else {
+            $error = "Username atau password salah!";
+        }
+    }
+?>
+
+
 <!DOCTYPE html>
 <html>
     <head>
