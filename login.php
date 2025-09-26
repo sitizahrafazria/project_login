f5169e76544392906232377a28b93b8788ee5583
sitizahrafazria <stzhrafzria@gmail.com>
2025-09-26 10:51:21 +0700

    mendesign form login

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <h2>From Login</h2>
+
+        <from sethod="pst">
+            username: <input type="text" nama="usename" required><br><br>
+            password: <input type="password" name="password" required><br><br>
+            <button type="submit">Login</button>
+        </from>
+    </body>
+</html>
